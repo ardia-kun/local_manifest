9268b634a1e5db3c7a57f995953cdf14152deb47
alternoegraha <noegrahachan@gmail.com>
2024-02-09 07:47:02 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,6 +16,9 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags && \
 # sync tree
 git clone -b 13-staging https://github.com/alternoegraha/device_xiaomi_fog device/xiaomi/fog && \
 
+# clone hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi && \
+
 # Set up build environment
 source build/envsetup.sh && \
 
